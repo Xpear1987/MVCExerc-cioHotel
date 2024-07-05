e08d964ce27771e8e0a9bdbb2faee06aa37ee49e
Alexandre Varisano Conceição <alexandre.conceicao@rdcviagens.com.br>
2024-07-05 09:26:54 -0300

    New pages added

---
+++ Migração_Exercicio_Hotel/Controllers/ConsultaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Migração_Exercicio_Hotel.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
